555aea34c3c1726c2cbdd188f9303382fcc90fb2
Justin Beynon <JMB335@gmail.com>
2020-05-27 22:43:32 -0500

    Completed get the titles

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,37 +1,42 @@
 const caesar = function(caesarWord, offset) {
 
-upperAlphaArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-lowerAlphaArray = 'abcdefghijklmnopqrstuvwxyz'.split('');
+upperAlphaArray = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''); //creates an arrray of all lowercase letters
+lowerAlphaArray = 'abcdefghijklmnopqrstuvwxyz'.split(''); //creates an arrray of all uppercase letters
 newString = "";
 
+//Deals with instances where the offset will loop back through the alphabet
 function alphaReset(indexFound, offset2){
 
-	if ((indexFound + offset2) >= 26){
-		let newIndex = ((indexFound + offset2) - 26);
-		return newIndex;
-	}
-	else if ((indexFound + offset2) <= 0){
-		let newIndex = ((indexFound + offset2) + 26);
-		return newIndex;
-	}
+let newNum = (indexFound + offset2)
 
-		else {return (indexFound + offset2)};
-	
-}
+if ((newNum) >= 26) {
+		
+		while (newNum >= 26){newNum -= 26}
+		return (newNum)
+	} 
+	else if ((newNum) < 0) {
+		
+		while (newNum < 0){newNum += 26}
+		return (newNum)
+	}
+	else {return newNum}
+	} 
 
 
 for (i=0; i < caesarWord.length; i++){
 
-	if (caesarWord.charAt(i) === ' ' || caesarWord.charAt(i) === ',' || caesarWord.charAt(i) === '!'){newString += caesarWord.charAt(i)}
+	if (caesarWord.charAt(i) === ' ' || 
+		caesarWord.charAt(i) === ',' || 
+		caesarWord.charAt(i) === '!'){newString += caesarWord.charAt(i)} //Checks to see if the character is not a letter, to ignore it
 
 else{
-	if (caesarWord.charAt(i) === caesarWord.charAt(i).toUpperCase()){
+	if (caesarWord.charAt(i) == caesarWord.charAt(i).toUpperCase()){ //Checks if the letter is uppercase
 	indexFinder = upperAlphaArray.indexOf(caesarWord.charAt(i))
 	newString += upperAlphaArray[alphaReset(indexFinder, offset)]
 	}
 
 	else {
-	indexFinder = lowerAlphaArray.indexOf(caesarWord.charAt(i))
+	indexFinder = lowerAlphaArray.indexOf(caesarWord.charAt(i)) //checks if the letter is lowercase
 	newString += lowerAlphaArray[alphaReset(indexFinder, offset)]
 		}
 	}
@@ -42,6 +47,5 @@ return newString;
 
 }
 
-caesar('A', 1)
 
 module.exports = caesar
--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+let result = array.map(a => a.title);
+return result;
 }
 
 module.exports = getTheTitles;
